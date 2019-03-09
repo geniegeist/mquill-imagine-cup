0fc09b909298793670a680298729e0796b10a0fe
Viet Duc <vduc.nguyen@icloud.com>
2019-03-09 09:25:13 +0100

    Design v3.0

---
+++ CambridgeTranscribe/Controller/Record/Views/RecordButton/RecordButton.swift
@@ -0,0 +1,21 @@
+//
+//  RecordButton.swift
+//  CambridgeTranscribe
+//
+//  Created by Viet Duc Nguyen on 08.03.19.
+//  Copyright © 2019 Viet Duc Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class RecordButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ CambridgeTranscribe/Utility/Color/UIColor+Hex.swift
@@ -0,0 +1,9 @@
+//
+//  UIColor+Hex.swift
+//  CambridgeTranscribe
+//
+//  Created by Viet Duc Nguyen on 08.03.19.
+//  Copyright © 2019 Viet Duc Nguyen. All rights reserved.
+//
+
+import Foundation
